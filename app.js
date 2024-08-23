c2be87c4e97c3d653fd4280e8eba5c9162ae3a97
HUNAIN882 <hunaintoufeeq@gmail.com>
2024-08-22 20:18:38 -0700

    Add new paragraph

--- app.js
+++ app.js
@@ -2,3 +2,7 @@ var age = "I am 17 years old"
 alert("age")
 var visited = "You have visited this site 14 time"
 alert("visited")
+var shop = "Daraz"
+var visitorname = "Hunain"
+var order = "5 shirt"
+var order = "5 pant"
