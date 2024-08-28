2426bf82307916f8c4cf56e9b5fa171a43290f93
HUNAIN882 <hunaintoufeeq@gmail.com>
2024-08-28 14:10:42 -0700

    Add new paragraph

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 var age = "I am 17 years old"
-alert("age")
+alert("age");
 var visited = "You have visited this site 14 time"
-alert("visited")
+alert("visited");
 var shop = "Daraz"
 var visitorname = "Hunain"
 var order = "5 shirt"
 var order2 = "5 pant"
-alert("shop")
-alert("visitorname")
+alert("shop");
+alert("order");
+alert(" order2");
